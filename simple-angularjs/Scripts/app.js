ebb32076debba1ea2ac6226c720dd59b7a3e2d6c
JaisEdelmann <jed@codesquare.dk>
2014-08-25 13:01:17 +0200

    addtional data added via promise api

--- simple-angularjs/Scripts/app.js
+++ simple-angularjs/Scripts/app.js
@@ -5,7 +5,7 @@ simpleapp.controller('HelloController', function ($scope) {
     $scope.description = "This is a simple AngularJS app, talking with Danmarks Radio MU API";
 });
 
-simpleapp.controller('TvController', function ($scope, $http) {
+simpleapp.controller('TvController', function ($scope, $http, $q) {
     $scope.channelsheadline = "Channels";
     $scope.IsBroadCasting = function (channel) {
         if (channel.Now)
@@ -16,18 +16,47 @@ simpleapp.controller('TvController', function ($scope, $http) {
         if (channel.Now)
             return false;
         return true;
-    }  
-    $http.get('http://www.dr.dk/mu-online/api/1.0/schedule/nownext-for-all-active-dr-tv-channels').success(function (data) {
-        for (var i = 0; i < data.length; i++) {
-            var CurrentTime =  parseInt(new Date().getTime().toString());
-            var StartTime = parseInt(new Date(data[i].Now.StartTime).getTime().toString());
-            var EndTime =  parseInt(new Date(data[i].Now.EndTime).getTime().toString());
+    }
+
+    var promiseEpgData = $http.get('http://www.dr.dk/mu-online/api/1.0/schedule/nownext-for-all-active-dr-tv-channels');
+    var promiseChannelData= $http.get('http://www.dr.dk/mu-online/api/1.0/channel/all-active-dr-tv-channels');
+
+    $q.all([promiseEpgData, promiseChannelData]).then(function (resultarray)
+    {
+        var epgdatalist = resultarray[0].data;
+        var channeldatalist = resultarray[1].data;
+        for (var i = 0; i < epgdatalist.length; i++) {
+            var test = $.grep(channeldatalist, function (e) {
+                return e.Slug == epgdatalist[i].ChannelSlug;
+            });
+
+            epgdatalist[i].ChannelInformation = test[0];
+            var CurrentTime = parseInt(new Date().getTime().toString());
+            var StartTime = parseInt(new Date(epgdatalist[i].Now.StartTime).getTime().toString());
+            var EndTime = parseInt(new Date(epgdatalist[i].Now.EndTime).getTime().toString());
             var a = (CurrentTime - StartTime);
             var b = (EndTime - StartTime);
-            data[i].procentage = Math.round((a / b * 100));
+            epgdatalist[i].procentage = Math.round((a / b * 100));
         }
-        $scope.channels = data;
-
         
+
+        $scope.channels = epgdatalist;
     });
+
+    
+
+    //$http.get('http://www.dr.dk/mu-online/api/1.0/schedule/nownext-for-all-active-dr-tv-channels').success(function (data) {
+    //    for (var i = 0; i < data.length; i++) {
+    //        var CurrentTime = parseInt(new Date().getTime().toString());
+    //        var StartTime = parseInt(new Date(data[i].Now.StartTime).getTime().toString());
+    //        var EndTime = parseInt(new Date(data[i].Now.EndTime).getTime().toString());
+    //        var a = (CurrentTime - StartTime);
+    //        var b = (EndTime - StartTime);
+    //        data[i].procentage = Math.round((a / b * 100));
+    //        data[i].channelinfo = [];
+    //    }
+    //    $scope.channels = data;
+    //});
+
+     
 });
